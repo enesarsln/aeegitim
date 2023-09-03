9ca04d9b739c46c4107db15fc8d7970722afbd20
denizcalp <139393076+denizcalp@users.noreply.github.com>
2023-09-03 22:40:27 +0300

    first commit

---
+++ pages/index-5.js
@@ -0,0 +1,39 @@
+import React from "react";
+import MainBanner from "../components/HomeFive/MainBanner";
+import SearchForm from "../components/HomeFive/SearchForm";
+import Facilities from "../components/HomeFive/Facilities";
+import AboutUs from "../components/HomeFive/AboutUs";
+import SpecialistDoctors from "../components/HomeFive/SpecialistDoctors";
+import DoctorsStyleOne from "../components/Common/DoctorsStyleOne";
+import NewsStyleOne from "../components/Common/NewsStyleOne";
+import Subscribe from "../components/Common/Subscribe";
+import Footer from "../components/_App/Footer";
+import FunFactStyleThree from "../components/Common/FunFactStyleThree";
+
+const Index5 = () => {
+  return (
+    <>
+      <MainBanner />
+
+      <SearchForm />
+
+      <Facilities />
+
+      <AboutUs />
+
+      <SpecialistDoctors />
+
+      <FunFactStyleThree />
+
+      <DoctorsStyleOne />
+
+      <Subscribe />
+
+      <NewsStyleOne />
+      
+      <Footer />
+    </>
+  );
+};
+
+export default Index5;
---
+++ pages/index-7.js
@@ -0,0 +1,42 @@
+import React from "react";
+import MainBanner from "../components/HomeSeven/MainBanner";
+import FeaturedService from "../components/HomeSeven/FeaturedService";
+import AboutUs from "../components/HomeSeven/AboutUs";
+import FunFact from "../components/HomeSeven/FunFact";
+import OurServices from "../components/HomeSeven/OurServices";
+import DoctorsStyleFour from "../components/Common/DoctorsStyleFour";
+import Faqs from "../components/Common/Faqs";
+import AppointmentForm from "../components/HomeSeven/AppointmentForm";
+import NewsStyleTwo from "../components/Common/NewsStyleTwo";
+import Subscribe from "../components/Common/Subscribe";
+import Footer from "../components/_App/Footer";
+
+const Index7 = () => {
+  return (
+    <>
+      <MainBanner />
+
+      <FeaturedService />
+
+      <AboutUs />
+
+      <FunFact />
+
+      <OurServices />
+
+      <DoctorsStyleFour />
+
+      <Faqs />
+
+      <AppointmentForm />
+
+      <NewsStyleTwo />
+
+      <Subscribe />
+
+      <Footer />
+    </>
+  );
+};
+
+export default Index7;
